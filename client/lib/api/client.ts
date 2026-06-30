10542b2d7ea29933d1a0bee2722cf109c00f783a
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2026-06-30 11:57:38 +0100

    fix: update apiClient baseURL to use environment variable and add type definitions for three.js

--- client/lib/api/client.ts
+++ client/lib/api/client.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
-// baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000/api/v1",
